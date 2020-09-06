8f86b2e96646f230c654bea0f52f0536ed013e77
Pichu Chen <pichu@tih.tw>
2020-09-06 10:17:11 +0800

    update formosa BBS

--- file.go
+++ file.go
@@ -19,19 +19,19 @@ import (
 )
 
 const (
-	FILE_LOCAL     = 0x01 /* local saved,  non-mail */
-	FILE_READ      = 0x01 /* already read, mail only */
-	FILE_MARKED    = 0x02 /* non-mail + mail */
-	FILE_DIGEST    = 0x04 /* digest,       non-mail */
-	FILE_REPLIED   = 0x04 /* replied,      mail only */
-	FILE_BOTTOM    = 0x08 /* push_bottom,  non-mail */
-	FILE_MULTI     = 0x08 /* multi send,   mail only */
-	FILE_SOLVED    = 0x10 /* problem solved, sysop/BM non-mail only */
-	FILE_HIDE      = 0x20 /* hide,	in announce */
-	FILE_BID       = 0x20 /* bid,		in non-announce */
-	FILE_BM        = 0x40 /* BM only,	in announce */
-	FILE_VOTE      = 0x40 /* for vote,	in non-announce */
-	FILE_ANONYMOUS = 0x80 /* anonymous file */
+	PTT_FILE_LOCAL     = 0x01 /* local saved,  non-mail */
+	PTT_FILE_READ      = 0x01 /* already read, mail only */
+	PTT_FILE_MARKED    = 0x02 /* non-mail + mail */
+	PTT_FILE_DIGEST    = 0x04 /* digest,       non-mail */
+	PTT_FILE_REPLIED   = 0x04 /* replied,      mail only */
+	PTT_FILE_BOTTOM    = 0x08 /* push_bottom,  non-mail */
+	PTT_FILE_MULTI     = 0x08 /* multi send,   mail only */
+	PTT_FILE_SOLVED    = 0x10 /* problem solved, sysop/BM non-mail only */
+	PTT_FILE_HIDE      = 0x20 /* hide,	in announce */
+	PTT_FILE_BID       = 0x20 /* bid,		in non-announce */
+	PTT_FILE_BM        = 0x40 /* BM only,	in announce */
+	PTT_FILE_VOTE      = 0x40 /* for vote,	in non-announce */
+	PTT_FILE_ANONYMOUS = 0x80 /* anonymous file */
 
 )
 const (
@@ -41,15 +41,15 @@ const (
 )
 
 const (
-	PosOfFilename  = 0
-	PosOfModified  = PosOfFilename + PTT_FNLEN
-	PosOfRecommend = 1 + PosOfModified + 4
-	PosOfOwner     = PosOfRecommend + 1
-	PosOfDate      = PosOfOwner + PTT_IDLEN + 2
-	PosOfTitle     = PosOfDate + 6
+	PosOfPTTFilename  = 0
+	PosOfPTTModified  = PosOfPTTFilename + PTT_FNLEN
+	PosOfPTTRecommend = 1 + PosOfPTTModified + 4
+	PosOfPTTOwner     = PosOfPTTRecommend + 1
+	PosOfPTTDate      = PosOfPTTOwner + PTT_IDLEN + 2
+	PosOfPTTTitle     = PosOfPTTDate + 6
 
-	PosOfUnionMulti = 1 + PosOfTitle + PTT_TTLEN + 1
-	PosOfFilemode   = PosOfUnionMulti + 4
+	PosOfPTTUnionMulti = 1 + PosOfPTTTitle + PTT_TTLEN + 1
+	PosOfPTTFilemode   = PosOfPTTUnionMulti + 4
 )
 
 type VoteLimits struct {
@@ -74,6 +74,8 @@ type FileHeader struct {
 	ReferFlag bool // 至底公告？
 
 	Filemode uint8
+
+	Postno int32 // FormosaBBS only
 }
 
 func OpenFileHeaderFile(filename string) ([]*FileHeader, error) {
@@ -109,27 +111,27 @@ func OpenFileHeaderFile(filename string) ([]*FileHeader, error) {
 func NewFileHeaderWithByte(data []byte) (*FileHeader, error) {
 
 	ret := FileHeader{}
-	ret.Filename = string(bytes.Trim(data[PosOfFilename:+PosOfFilename+PTT_FNLEN], "\x00"))
+	ret.Filename = string(bytes.Trim(data[PosOfPTTFilename:+PosOfPTTFilename+PTT_FNLEN], "\x00"))
 
-	modifiedInt := binary.LittleEndian.Uint32(data[PosOfModified : PosOfModified+4])
+	modifiedInt := binary.LittleEndian.Uint32(data[PosOfPTTModified : PosOfPTTModified+4])
 	ret.Modified = time.Unix(int64(modifiedInt), 0)
 
-	ret.Recommend = int8(data[PosOfRecommend])
-	ret.Owner = string(bytes.Trim(data[PosOfOwner:PosOfOwner+PTT_IDLEN+2], "\x00"))
-	ret.Date = string(bytes.Trim(data[PosOfDate:PosOfDate+6], "\x00"))
-	ret.Title = Big5ToUtf8(string(bytes.Trim(data[PosOfTitle:PosOfTitle+PTT_TTLEN+1], "\x00")))
+	ret.Recommend = int8(data[PosOfPTTRecommend])
+	ret.Owner = string(bytes.Trim(data[PosOfPTTOwner:PosOfPTTOwner+PTT_IDLEN+2], "\x00"))
+	ret.Date = string(bytes.Trim(data[PosOfPTTDate:PosOfPTTDate+6], "\x00"))
+	ret.Title = Big5ToUtf8(string(bytes.Trim(data[PosOfPTTTitle:PosOfPTTTitle+PTT_TTLEN+1], "\x00")))
 	// log.Println("PosOfUnionMulti:", PosOfUnionMulti, data[PosOfUnionMulti])
 
-	ret.Money = int(binary.LittleEndian.Uint32(data[PosOfUnionMulti : PosOfUnionMulti+4]))
-	ret.AnnoUid = int(binary.LittleEndian.Uint32(data[PosOfUnionMulti : PosOfUnionMulti+4]))
+	ret.Money = int(binary.LittleEndian.Uint32(data[PosOfPTTUnionMulti : PosOfPTTUnionMulti+4]))
+	ret.AnnoUid = int(binary.LittleEndian.Uint32(data[PosOfPTTUnionMulti : PosOfPTTUnionMulti+4]))
 
-	ret.Filemode = uint8(data[PosOfFilemode])
+	ret.Filemode = uint8(data[PosOfPTTFilemode])
 
 	if ret.IsVotePost() {
-		ret.VoteLimits.Posts = data[PosOfUnionMulti+0]
-		ret.VoteLimits.Logins = data[PosOfUnionMulti+1]
-		ret.VoteLimits.Regtime = data[PosOfUnionMulti+2]
-		ret.VoteLimits.Badpost = data[PosOfUnionMulti+3]
+		ret.VoteLimits.Posts = data[PosOfPTTUnionMulti+0]
+		ret.VoteLimits.Logins = data[PosOfPTTUnionMulti+1]
+		ret.VoteLimits.Regtime = data[PosOfPTTUnionMulti+2]
+		ret.VoteLimits.Badpost = data[PosOfPTTUnionMulti+3]
 	}
 
 	// ret.Title = binary.LittleEndian.Uint8(data[PTT_FNLEN+5+PTT_IDLEN+2+6 : PTT_FNLEN+5+PTT_IDLEN+2+6+PTT_TTLEN+1])
@@ -140,13 +142,13 @@ func NewFileHeaderWithByte(data []byte) (*FileHeader, error) {
 func (h *FileHeader) MarshalToByte() ([]byte, error) {
 	ret := make([]byte, 128)
 
-	copy(ret[PosOfFilename:PosOfFilename+PTT_FNLEN], h.Filename)
-	binary.LittleEndian.PutUint32(ret[PosOfModified:PosOfModified+4], uint32(h.Modified.Unix()))
+	copy(ret[PosOfPTTFilename:PosOfPTTFilename+PTT_FNLEN], h.Filename)
+	binary.LittleEndian.PutUint32(ret[PosOfPTTModified:PosOfPTTModified+4], uint32(h.Modified.Unix()))
 
-	ret[PosOfRecommend] = byte(h.Recommend)
-	copy(ret[PosOfOwner:PosOfOwner+PTT_IDLEN+2], h.Owner)
-	copy(ret[PosOfDate:PosOfDate+6], h.Date)
-	copy(ret[PosOfTitle:PosOfTitle+PTT_TTLEN+1], Utf8ToBig5(h.Title))
+	ret[PosOfPTTRecommend] = byte(h.Recommend)
+	copy(ret[PosOfPTTOwner:PosOfPTTOwner+PTT_IDLEN+2], h.Owner)
+	copy(ret[PosOfPTTDate:PosOfPTTDate+6], h.Date)
+	copy(ret[PosOfPTTTitle:PosOfPTTTitle+PTT_TTLEN+1], Utf8ToBig5(h.Title))
 
 	// TODO: Check file mode for set Money or AnnoUid ... etc
 
@@ -154,5 +156,5 @@ func (h *FileHeader) MarshalToByte() ([]byte, error) {
 }
 
 func (h *FileHeader) IsVotePost() bool {
-	return h.Filemode&FILE_VOTE != 0
+	return h.Filemode&PTT_FILE_VOTE != 0
 }
---
+++ file_formosabbs.go
@@ -0,0 +1,83 @@
+package bbs
+
+import (
+	"bytes"
+	"encoding/binary"
+	"io"
+	"log"
+	"os"
+	"time"
+)
+
+const (
+	FORMOSABBS_STRLEN = 80
+
+	PosOfFormosaBBSFilename = 0
+	PosOfFormosaBBSOwner    = FORMOSABBS_STRLEN
+	PosOfFormosaBBSPostno   = FORMOSABBS_STRLEN - 8
+	PosOfFormosaBBSModified = PosOfFormosaBBSOwner + FORMOSABBS_STRLEN - 8
+	PosOfFormosaBBSTitle    = PosOfFormosaBBSOwner + FORMOSABBS_STRLEN
+)
+
+func OpenFormosaBBSFileHeaderFile(filename string) ([]*FileHeader, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	ret := []*FileHeader{}
+
+	for {
+		hdr := make([]byte, 248)
+		_, err := file.Read(hdr)
+		// log.Println(len, err)
+		if err == io.EOF {
+			break
+		}
+
+		f, err := NewFomosaBBSFileHeaderWithByte(hdr)
+		if err != nil {
+			return nil, err
+		}
+		ret = append(ret, f)
+		// log.Println(f.Filename)
+
+	}
+
+	return ret, nil
+
+}
+
+func NewFomosaBBSFileHeaderWithByte(data []byte) (*FileHeader, error) {
+
+	ret := FileHeader{}
+	ret.Filename = string(bytes.Trim(data[PosOfFormosaBBSFilename:+PosOfFormosaBBSFilename+44], "\x00"))
+
+	modifiedInt := binary.LittleEndian.Uint32(data[PosOfFormosaBBSModified : PosOfFormosaBBSModified+4])
+	// log.Println("modifiedInt:", modifiedInt, PosOfFormosaBBSModified)
+	ret.Modified = time.Unix(int64(modifiedInt), 0)
+
+	// ret.Recommend = int8(data[PosOfRecommend])
+	ret.Owner = string(bytes.Trim(data[PosOfFormosaBBSOwner:PosOfFormosaBBSOwner+72], "\x00"))
+	// ret.Date = string(bytes.Trim(data[PosOfDate:PosOfDate+6], "\x00"))
+	ret.Title = Big5ToUtf8(string(bytes.Trim(data[PosOfFormosaBBSTitle:PosOfFormosaBBSTitle+67], "\x00")))
+	// // log.Println("PosOfUnionMulti:", PosOfUnionMulti, data[PosOfUnionMulti])
+
+	// ret.Money = int(binary.LittleEndian.Uint32(data[PosOfUnionMulti : PosOfUnionMulti+4]))
+	// ret.AnnoUid = int(binary.LittleEndian.Uint32(data[PosOfUnionMulti : PosOfUnionMulti+4]))
+
+	// ret.Filemode = uint8(data[PosOfFilemode])
+
+	// if ret.IsVotePost() {
+	// 	ret.VoteLimits.Posts = data[PosOfUnionMulti+0]
+	// 	ret.VoteLimits.Logins = data[PosOfUnionMulti+1]
+	// 	ret.VoteLimits.Regtime = data[PosOfUnionMulti+2]
+	// 	ret.VoteLimits.Badpost = data[PosOfUnionMulti+3]
+	// }
+
+	ret.Postno = int32(binary.LittleEndian.Uint32(data[PosOfFormosaBBSPostno : PosOfFormosaBBSPostno+4]))
+	// ret.Title = binary.LittleEndian.Uint8(data[PTT_FNLEN+5+PTT_IDLEN+2+6 : PTT_FNLEN+5+PTT_IDLEN+2+6+PTT_TTLEN+1])
+
+	return &ret, nil
+}

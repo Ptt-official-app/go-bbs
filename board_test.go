29163edab152ce9d1389768e7b0f43ac8c960d58
Paul Tang <cftang0827@gmail.com>
2020-11-16 01:34:46 +0800

    add first expected in board_test.go

--- board_test.go
+++ board_test.go
@@ -2,12 +2,52 @@ package bbs
 
 import (
 	"testing"
+	"time"
 )
 
 func TestSomething(t *testing.T) {
-	_, err := OpenBoardHeaderFile("testcase/board/00.BRD")
+	headers, err := OpenBoardHeaderFile("testcase/board/01.BRD")
 	if err != nil {
 		t.Error(err)
+	}
+
+	expected := []BoardHeader{
+		{
+			BrdName:            "SYSOP",
+			Title:              "嘰哩 ◎站長好!",
+			BM:                 "",
+			Brdattr:            PTT_BRD_POSTMASK,
+			VoteLimitPosts:     0,
+			VoteLimitLogins:    0,
+			ChessCountry:       "",
+			BUpdate:            time.Unix(int64(0), 0),
+			PostLimitPosts:     0,
+			PostLimitLogins:    0,
+			BVote:              0,
+			VTime:              time.Unix(int64(0), 0),
+			Level:              0,
+			PermReload:         time.Unix(int64(0), 0),
+			Gid:                2,
+			Next:               []int32{0, 0},
+			FirstChild:         []int32{0, 0},
+			Parent:             0,
+			ChildCount:         0,
+			Nuser:              0,
+			PostExpire:         0,
+			EndGamble:          time.Unix(int64(0), 0),
+			PostType:           "",
+			PostTypeF:          "",
+			FastRecommendPause: 0,
+			VoteLimitBadPost:   0,
+			PostLimitBadPost:   0,
+			SRexpire:           time.Unix(int64(0), 0),
+		},
+	}
+
+	for index, header := range headers {
+		if header.BrdName != expected[index].BrdName {
+			t.Errorf("Board name not match in index %d, expected: %s, got: %s", index, expected[index].BrdName, header.BrdName)
+		}
 	}
 
 }

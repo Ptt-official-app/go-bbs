cbee4e20cc79d8647fc1d5689d6f36cb6471f0a6
Pichu Chen <pichu@tih.tw>
2020-12-12 02:52:16 +0800

    impliment some code for #7

--- board.go
+++ board.go
@@ -5,3 +5,63 @@ package bbs
 // Please see https://github.com/ptt/pttbbs/blob/master/include/pttstruct.h
 // boardheader_t
 //
+
+import (
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+type BoardHeader struct {
+	Boardname       string
+	Title           string
+	BoardModerators []string
+}
+
+func OpenBoardHeaderFile(filename string) ([]*BoardHeader, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	ret := []*BoardHeader{}
+
+	for {
+		hdr := make([]byte, 256)
+		_, err := file.Read(hdr)
+		// log.Println(len, err)
+		if err == io.EOF {
+			break
+		}
+
+		f, err := NewBoardHeaderWithByte(hdr)
+		if err != nil {
+			return nil, err
+		}
+		ret = append(ret, f)
+		// log.Println(f.Filename)
+
+	}
+
+	return ret, nil
+
+}
+
+func NewBoardHeaderWithByte(data []byte) (*BoardHeader, error) {
+
+	const PTT_BTLEN = 48
+
+	var posOfPTTBrdname = 0
+	var posOfTitle = posOfPTTBrdname + PTT_IDLEN + 1
+	var posOfBM = posOfTitle + PTT_BTLEN + 1
+	// var posOfPTTBrdname = posOfPTTBrdname + PTT_IDLEN
+
+	ret := BoardHeader{}
+	ret.Boardname = string(CstrToBytes(data[posOfPTTBrdname : +posOfPTTBrdname+PTT_IDLEN+1]))
+	ret.Title = Big5ToUtf8(CstrToBytes(data[posOfTitle : posOfTitle+PTT_BTLEN+1]))
+	ret.BoardModerators = strings.Split(Big5ToUtf8(CstrToBytes(data[posOfBM:posOfBM+3*PTT_IDLEN+3])), "/")
+
+	return &ret, nil
+}

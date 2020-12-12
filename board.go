db48a7fbc0569cda80b716b32885506f42a39a1b
Pichu Chen <pichu@tih.tw>
2020-12-12 16:35:03 +0800

    add board attr flag parse

--- board.go
+++ board.go
@@ -7,17 +7,69 @@ package bbs
 //
 
 import (
+	"encoding/binary"
 	"io"
 	"log"
 	"os"
 	"strings"
+	"time"
 )
 
 type BoardHeader struct {
 	Boardname       string
 	Title           string
 	BoardModerators []string
+	Brdattr         uint32 // raw value
+	Chesscountry    uint8
+	VoteLimitLogins uint8
+	BoardUpdate     time.Time
+	PostLimitLogins uint8
+	BoardVote       uint8
+	VoteTime        time.Time // Vote close time
+	Level           uint32
+	PermReload      time.Time
+	Gid             int32
+	// Next [2]int32
+	// FirstChild [2]int32
+	Parent              int32
+	ChildCount          int32
+	Nuser               int32 // How many user in this board
+	PostExpire          int32 // ?
+	EndGamble           time.Time
+	posttype            [33]uint8
+	postTypeF           uint8
+	FastRecommendPuause int // Delay for continuous recommend (Push)
+	VoteLimitBadpost    uint8
+	PostLimitBadpost    uint8
+	SRExpire            time.Time
 }
+
+func (b *BoardHeader) IsNoCount() bool          { return b.Brdattr&0x00000002 != 0 }
+func (b *BoardHeader) IsGroudBoard() bool       { return b.Brdattr&0x00000008 != 0 } // Class
+func (b *BoardHeader) IsHide() bool             { return b.Brdattr&0x00000010 != 0 } // Hide board or friend only
+func (b *BoardHeader) IsPostMask() bool         { return b.Brdattr&0x00000020 != 0 } // Has Post or Reading Limition
+func (b *BoardHeader) IsAnonymous() bool        { return b.Brdattr&0x00000040 != 0 }
+func (b *BoardHeader) IsDefaultAnonymous() bool { return b.Brdattr&0x00000080 != 0 }
+func (b *BoardHeader) IsNoCredit() bool         { return b.Brdattr&0x00000100 != 0 }
+func (b *BoardHeader) IsVoteBoard() bool        { return b.Brdattr&0x00000200 != 0 }
+func (b *BoardHeader) IsWarnEL() bool           { return b.Brdattr&0x00000400 != 0 } // Warning for Remove Board
+func (b *BoardHeader) IsTop() bool              { return b.Brdattr&0x00000800 != 0 }
+func (b *BoardHeader) IsNoRecommend() bool      { return b.Brdattr&0x00001000 != 0 } // Forbiddent Recommend (Push)
+func (b *BoardHeader) IsAngelAnonymous() bool   { return b.Brdattr&0x00002000 != 0 }
+func (b *BoardHeader) IsBMCount() bool          { return b.Brdattr&0x00004000 != 0 }
+func (b *BoardHeader) IsIsSymbolic() bool       { return b.Brdattr&0x00008000 != 0 } // symbolic link to board
+func (b *BoardHeader) IsNoBoo() bool            { return b.Brdattr&0x00010000 != 0 }
+func (b *BoardHeader) IsRestrictedPost() bool   { return b.Brdattr&0x00040000 != 0 } // Board Friend only
+func (b *BoardHeader) IsGuestPost() bool        { return b.Brdattr&0x00080000 != 0 }
+func (b *BoardHeader) IsCooldown() bool         { return b.Brdattr&0x00100000 != 0 }
+func (b *BoardHeader) IsCPLog() bool            { return b.Brdattr&0x00200000 != 0 }
+func (b *BoardHeader) IsNoFastRecommend() bool  { return b.Brdattr&0x00400000 != 0 }
+func (b *BoardHeader) IsIPLogRecommend() bool   { return b.Brdattr&0x00800000 != 0 }
+func (b *BoardHeader) IsOver18() bool           { return b.Brdattr&0x01000000 != 0 }
+func (b *BoardHeader) IsNoReply() bool          { return b.Brdattr&0x02000000 != 0 }
+func (b *BoardHeader) IsAlignedComment() bool   { return b.Brdattr&0x04000000 != 0 }
+func (b *BoardHeader) IsNoSelfDeletePost() bool { return b.Brdattr&0x08000000 != 0 }
+func (b *BoardHeader) IsBMMaskContent() bool    { return b.Brdattr&0x10000000 != 0 }
 
 func OpenBoardHeaderFile(filename string) ([]*BoardHeader, error) {
 	file, err := os.Open(filename)
@@ -56,12 +108,14 @@ func NewBoardHeaderWithByte(data []byte) (*BoardHeader, error) {
 	var posOfPTTBrdname = 0
 	var posOfTitle = posOfPTTBrdname + PTT_IDLEN + 1
 	var posOfBM = posOfTitle + PTT_BTLEN + 1
+	var posOfBrdattr = posOfBM + 3*PTT_IDLEN + 3 + 3
 	// var posOfPTTBrdname = posOfPTTBrdname + PTT_IDLEN
 
 	ret := BoardHeader{}
 	ret.Boardname = string(CstrToBytes(data[posOfPTTBrdname : +posOfPTTBrdname+PTT_IDLEN+1]))
 	ret.Title = Big5ToUtf8(CstrToBytes(data[posOfTitle : posOfTitle+PTT_BTLEN+1]))
 	ret.BoardModerators = strings.Split(Big5ToUtf8(CstrToBytes(data[posOfBM:posOfBM+3*PTT_IDLEN+3])), "/")
+	ret.Brdattr = binary.LittleEndian.Uint32(data[posOfBrdattr : posOfBrdattr+4])
 
 	return &ret, nil
 }
--- board_test.go
+++ board_test.go
@@ -17,71 +17,85 @@ func TestParseBoardHeader(t *testing.T) {
 			Boardname:       "SYSOP",
 			Title:           "嘰哩 ◎站長好!",
 			BoardModerators: []string{""},
+			Brdattr:         0x00000020,
 		},
 		{
 			Boardname:       "1...........",
 			Title:           ".... Σ中央政府  《高壓危險,非人可敵》",
 			BoardModerators: []string{""},
+			Brdattr:         0x00000008,
 		},
 		{
 			Boardname:       "junk",
 			Title:           "發電 ◎雜七雜八的垃圾",
 			BoardModerators: []string{""},
+			Brdattr:         0x00000000,
 		},
 		{
 			Boardname:       "Security",
 			Title:           "發電 ◎站內系統安全",
 			BoardModerators: []string{""},
+			Brdattr:         0x00000000,
 		},
 		{
 			Boardname:       "2...........",
 			Title:           ".... Σ市民廣場     報告  站長  ㄜ！",
 			BoardModerators: []string{""},
+			Brdattr:         0x00000008,
 		},
 		{
 			Boardname:       "ALLPOST",
 			Title:           "嘰哩 ◎跨板式LOCAL新文章",
 			BoardModerators: []string{""},
+			Brdattr:         0x00000020,
 		},
 		{
 			Boardname:       "deleted",
 			Title:           "嘰哩 ◎資源回收筒",
 			BoardModerators: []string{""},
+			Brdattr:         0x00000000,
 		},
 		{
 			Boardname:       "Note",
 			Title:           "嘰哩 ◎動態看板及歌曲投稿",
 			BoardModerators: []string{""},
+			Brdattr:         0x00000000,
 		},
 		{
 			Boardname:       "Record",
 			Title:           "嘰哩 ◎我們的成果",
 			BoardModerators: []string{""},
+			Brdattr:         0x00000020,
 		},
 		{
 			Boardname:       "WhoAmI",
 			Title:           "嘰哩 ◎呵呵，猜猜我是誰！",
 			BoardModerators: []string{""},
+			Brdattr:         0x00000000,
 		},
 		{
 			Boardname:       "EditExp",
 			Title:           "嘰哩 ◎範本精靈投稿區",
 			BoardModerators: []string{""},
+			Brdattr:         0x00000000,
 		},
 		{
 			Boardname:       "ALLHIDPOST",
 			Title:           "嘰哩 ◎跨板式LOCAL新文章(隱板)",
 			BoardModerators: []string{""},
+			Brdattr:         0x00000030,
 		},
 		{
 			Boardname:       "ptt_app",
 			Title:           "測試 ◎大家來玩吧",
 			BoardModerators: []string{"SYSOP"},
+			Brdattr:         0x00280020,
 		},
 		{
 			Boardname:       "test",
 			Title:           "測試 ◎測試測試測試",
 			BoardModerators: []string{"SYSOP"},
+			Brdattr:         0x00280020,
 		},
 	}
 
@@ -97,6 +111,9 @@ func TestParseBoardHeader(t *testing.T) {
 		if strings.Join(header.BoardModerators, "}{") != strings.Join(expected[index].BoardModerators, "}{") {
 			t.Logf("len :%d %d", len(header.BoardModerators), len(expected[index].BoardModerators))
 			t.Errorf("BoardModerators not match in index %d, expected: %s, got: %s", index, expected[index].BoardModerators, header.BoardModerators)
+		}
+		if header.Brdattr != expected[index].Brdattr {
+			t.Errorf("Raw Brdattr not match in index %d, expected: %08X, got: %08X", index, expected[index].Brdattr, header.Brdattr)
 		}
 
 	}

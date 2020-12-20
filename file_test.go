56cd35df8077280b6f05de521054e9b72383c6f8
Pichu Chen <pichu@tih.tw>
2020-12-20 19:18:29 +0800

    test for treasure DIR file

--- file_test.go
+++ file_test.go
@@ -8,7 +8,6 @@ import (
 )
 
 func TestParseFileHeader(t *testing.T) {
-
 	headers, err := OpenFileHeaderFile("testcase/file/01.DIR")
 	if err != nil {
 		t.Error(err)
@@ -78,45 +77,115 @@ func TestParseFileHeader(t *testing.T) {
 	}
 
 	for index, header := range headers {
-		if header.Filename != expected[index].Filename {
-			t.Logf("lena :%d %d", len(header.Filename), len(expected[index].Filename))
-			t.Errorf("Filename not match in index %d, expected: %s, got: %s", index, expected[index].Filename, header.Filename)
-		}
+		CheckPttFileHeader(t, index, header, &expected[index])
+	}
+}
 
-		if header.Modified.Sub(expected[index].Modified) != 0 {
-			t.Errorf("Modified not match in index %d, expected: %s, got: %s", index, expected[index].Modified, header.Modified)
-		}
-		if header.Recommend != expected[index].Recommend {
-			t.Errorf("Recommend not match in index %d, expected: %q, got: %q", index, expected[index].Recommend, header.Recommend)
-		}
-		if header.Owner != expected[index].Owner {
-			t.Errorf("Owner not match in index %d, expected: %s, got: %s", index, expected[index].Owner, header.Owner)
-		}
-		if header.Date != expected[index].Date {
-			t.Logf("Date :%d %d", len(header.Date), len(expected[index].Date))
-			t.Errorf("Date not match in index %d, expected: %q, got: %q", index, expected[index].Date, header.Date)
-		}
-		if header.Title != expected[index].Title {
-			t.Errorf("Title not match in index %d, expected: %q, got: %q", index, expected[index].Title, header.Title)
-		}
-		if header.Money != expected[index].Money {
-			t.Errorf("Money not match in index %d, expected: %q, got: %q", index, expected[index].Money, header.Money)
-		}
-		if header.AnnoUid != expected[index].AnnoUid {
-			t.Errorf("AnnoUid not match in index %d, expected: %q, got: %q", index, expected[index].AnnoUid, header.AnnoUid)
-		}
-		if header.VoteLimits != expected[index].VoteLimits {
-			t.Errorf("VoteLimits not match in index %d, expected: %q, got: %q", index, expected[index].VoteLimits, header.VoteLimits)
-		}
-		if header.ReferRef != expected[index].ReferRef {
-			t.Errorf("ReferRef not match in index %d, expected: %q, got: %q", index, expected[index].ReferRef, header.ReferRef)
-		}
-		if header.Filemode != expected[index].Filemode {
-			t.Errorf("Filemode not match in index %d, expected: %q, got: %q", index, expected[index].Filemode, header.Filemode)
-		}
-
+func TestParseFileHeader02(t *testing.T) {
+	headers, err := OpenFileHeaderFile("testcase/file/02.DIR")
+	if err != nil {
+		t.Error(err)
 	}
 
+	expected := []FileHeader{
+		{
+			Filename:  "M.1604489415.A.C31",
+			Modified:  time.Date(2020, 11, 04, 11, 30, 14, 0, time.UTC),
+			Recommend: 0,
+			Owner:     "pichu",
+			Date:      "11/04",
+			Title:     "[問題] test",
+
+			Money:   0,
+			AnnoUid: 0,
+			VoteLimits: VoteLimits{
+				Posts:   0,
+				Logins:  0,
+				Regtime: 0,
+				Badpost: 0,
+			},
+			ReferRef:  0,
+			ReferFlag: true,
+			Filemode:  0,
+		},
+	}
+
+	for index, header := range headers {
+		CheckPttFileHeader(t, index, header, &expected[index])
+	}
+}
+func TestParsePttFileHeaderTreasures(t *testing.T) {
+	headers, err := OpenFileHeaderFile("testcase/file/03.DIR")
+	if err != nil {
+		t.Error(err)
+	}
+
+	expected := []FileHeader{
+		{
+			Filename:  "D6D8",
+			Modified:  time.Date(1970, 1, 1, 0, 0, 0, 0, time.UTC),
+			Recommend: 0,
+			Owner:     "SYSOP",
+			Date:      "12/20",
+			Title:     "◆ Folder 1.1.1.1",
+
+			Money:   0,
+			AnnoUid: 0,
+			VoteLimits: VoteLimits{
+				Posts:   0,
+				Logins:  0,
+				Regtime: 0,
+				Badpost: 0,
+			},
+			ReferRef:  0,
+			ReferFlag: true,
+			Filemode:  0,
+		},
+	}
+
+	for index, header := range headers {
+		CheckPttFileHeader(t, index, header, &expected[index])
+	}
+}
+
+func CheckPttFileHeader(t *testing.T, index int, actual *FileHeader, expected *FileHeader) {
+
+	if actual.Filename != expected.Filename {
+		t.Logf("lena :%d %d", len(actual.Filename), len(expected.Filename))
+		t.Errorf("Filename not match in index %d, expected: %s, got: %s", index, expected.Filename, actual.Filename)
+	}
+
+	if actual.Modified.Sub(expected.Modified) != 0 {
+		t.Errorf("Modified not match in index %d, expected: %s, got: %s", index, expected.Modified, actual.Modified)
+	}
+	if actual.Recommend != expected.Recommend {
+		t.Errorf("Recommend not match in index %d, expected: %q, got: %q", index, expected.Recommend, actual.Recommend)
+	}
+	if actual.Owner != expected.Owner {
+		t.Errorf("Owner not match in index %d, expected: %s, got: %s", index, expected.Owner, actual.Owner)
+	}
+	if actual.Date != expected.Date {
+		t.Logf("Date :%d %d", len(actual.Date), len(expected.Date))
+		t.Errorf("Date not match in index %d, expected: %q, got: %q", index, expected.Date, actual.Date)
+	}
+	if actual.Title != expected.Title {
+		t.Errorf("Title not match in index %d, expected: %q, got: %q", index, expected.Title, actual.Title)
+	}
+	if actual.Money != expected.Money {
+		t.Errorf("Money not match in index %d, expected: %q, got: %q", index, expected.Money, actual.Money)
+	}
+	if actual.AnnoUid != expected.AnnoUid {
+		t.Errorf("AnnoUid not match in index %d, expected: %q, got: %q", index, expected.AnnoUid, actual.AnnoUid)
+	}
+	if actual.VoteLimits != expected.VoteLimits {
+		t.Errorf("VoteLimits not match in index %d, expected: %q, got: %q", index, expected.VoteLimits, actual.VoteLimits)
+	}
+	if actual.ReferRef != expected.ReferRef {
+		t.Errorf("ReferRef not match in index %d, expected: %q, got: %q", index, expected.ReferRef, actual.ReferRef)
+	}
+	if actual.Filemode != expected.Filemode {
+		t.Errorf("Filemode not match in index %d, expected: %q, got: %q", index, expected.Filemode, actual.Filemode)
+	}
 }
 
 func TestEncodingFileHeader(t *testing.T) {

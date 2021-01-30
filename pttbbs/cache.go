418086b86c8c71c10ac5ee94b1c2c6f4db3ae4f5
Pichu Chen <pichu@tih.tw>
2021-01-30 18:02:31 +0800

    add comment for cache

--- pttbbs/cache.go
+++ pttbbs/cache.go
@@ -32,40 +32,6 @@ type cachePos struct {
 	posOfUTMPNeedSort  int
 	posOfUTMPBusyState int
 
-	// 	int   version;  // SHM_VERSION   for verification // 0, 4
-	//     int   size;	    // sizeof(SHM_t) for verification // 4, 4
-
-	//     /* uhash */
-	//     /* uhash is a userid->uid hash table -- jochang */
-	//     char    userid[MAX_USERS][IDLEN + 1]; // 8, 650
-	//     char    gap_1[IDLEN+1]; // 658, 13
-	//     int     next_in_hash[MAX_USERS]; // 672, 200
-	//     char    gap_2[sizeof(int)]; //
-	//     int     money[MAX_USERS]; //    ,200
-	//     char    gap_3[sizeof(int)];
-	//     // TODO(piaip) Always have this var - no more #ifdefs in structure.
-	// #ifdef USE_COOLDOWN
-	//     time4_t cooldowntime[MAX_USERS];
-	// #endif
-	//     char    gap_4[sizeof(int)];
-	//     int     hash_head[1 << HASH_BITS];
-	//     char    gap_5[sizeof(int)];
-	//     int     number;				/* # of users total */
-	//     int     loaded;				/* .PASSWD has been loaded? */
-
-	//     /* utmpshm */
-	//     userinfo_t      uinfo[USHM_SIZE]; // 有 NOKILLWATERBALL: 3484,
-	//     char    gap_6[sizeof(userinfo_t)];
-	//     int             sorted[2][9][USHM_SIZE]; // 朋友列表
-	//                     /* 第一維double buffer 由currsorted指向目前使用的
-	// 		       第二維sort type */
-	//     char    gap_7[sizeof(int)];
-	//     int     currsorted;
-	//     time4_t UTMPuptime;
-	//     int     UTMPnumber;
-	//     char    UTMPneedsort;
-	//     char    UTMPbusystate;
-
 	posOfBMCache     int
 	posOfBoardCache  int
 	posOfSortedBoard int
@@ -85,47 +51,6 @@ type cachePos struct {
 	posOfNotes   int
 	posOfTodayIs int
 
-	//     /* brdshm */
-	//     char    gap_8[sizeof(int)];
-	//     int     BMcache[MAX_BOARD][MAX_BMs];
-	//     char    gap_9[sizeof(int)];
-	//     boardheader_t   bcache[MAX_BOARD];
-	//     char    gap_10[sizeof(int)];
-	//     int     bsorted[2][MAX_BOARD]; /* 0: by name 1: by class */ /* 裡頭存的是 bid-1 */
-	//     char    gap_11[sizeof(int)];
-	//     // TODO(piaip) Always have this var - no more #ifdefs in structure.
-	// #if HOTBOARDCACHE
-	//     unsigned char    nHOTs;
-	//     int              HBcache[HOTBOARDCACHE];
-	// #endif
-	//     char    gap_12[sizeof(int)];
-	//     time4_t busystate_b[MAX_BOARD];
-	//     char    gap_13[sizeof(int)];
-	//     int     total[MAX_BOARD];
-	//     char    gap_14[sizeof(int)];
-	//     unsigned char  n_bottom[MAX_BOARD]; /* number of bottom */
-	//     char    gap_15[sizeof(int)];
-	//     int     hbfl[MAX_BOARD][MAX_FRIEND + 1]; /* hidden board friend list, 0: load time, 1-MAX_FRIEND: uid */
-	//     char    gap_16[sizeof(int)];
-	//     time4_t lastposttime[MAX_BOARD];
-	//     char    gap_17[sizeof(int)];
-	//     time4_t Buptime;
-	//     time4_t Btouchtime;
-	//     int     Bnumber;
-	//     int     Bbusystate;
-	//     time4_t close_vote_time;
-
-	//     /* pttcache */
-	//     char    notes[MAX_ADBANNER][256*MAX_ADBANNER_HEIGHT];
-	//     char    gap_18[sizeof(int)];
-	//     char    today_is[20];
-	// FIXME remove it
-	// int     __never_used__n_notes[MAX_ADBANNER_SECTION];      /* 一節中有幾個 看板 */
-	// char    gap_19[sizeof(int)];
-	// // FIXME remove it
-	// int     __never_used__next_refresh[MAX_ADBANNER_SECTION]; /* 下一次要refresh的 看板 */
-	// char    gap_20[sizeof(int)];
-
 	posOfLoginMsg   int
 	posOfLastFilm   int
 	posOflastUSong  int
@@ -133,51 +58,17 @@ type cachePos struct {
 	posOfPTouchTime int
 	posOfPBusyState int
 
-	//    msgque_t loginmsg;  /* 進站水球 */
-	//    int     last_film;
-	//    int     last_usong;
-	//    time4_t Puptime;
-	//    time4_t Ptouchtime;
-	//    int     Pbusystate;
-
-	//    /* SHM 中的全域變數, 可用 shmctl 設定或顯示. 供動態調整或測試使用 */
-	//    union {
-	// int     v[512];
-	// struct {
-	//     int     dymaxactive;  /* 動態設定最大人數上限     */
-	//     int     toomanyusers; /* 超過人數上限不給進的個數 */
-	//     int     noonlineuser; /* 站上使用者不高亮度顯示   */
-	//     time4_t now __attribute__ ((deprecated));
-	//     int     nWelcomes;
-	//     int     shutdown;     /* shutdown flag */
-
-	//     /* 注意, 應保持 align sizeof(int) */
-	// } e;
-	//    } GV2;
-	/* statistic */
-	// unsigned int    statistic[STAT_MAX];
-
 	posOfStatistic int
-
-	// 從前作為故鄉使用 (fromcache). 現已被 daemon/fromd 取代。
-	// unsigned int    _deprecated_home_ip[MAX_FROM];
-	// unsigned int    _deprecated_home_mask[MAX_FROM];
-	// char            _deprecated_home_desc[MAX_FROM][32];
-	// int        	    _deprecated_home_num;
 
 	posOfMaxUser   int
 	posOfMaxTime   int
 	posOfFUptime   int
 	posOfTouchTime int
 	posOfBusyState int
-
-	// int     max_user
-	// time4_t max_time
-	// time4_t Fuptime
-	// time4_t Ftouchtime
-	// int     Fbusystate
 }
 
+// MemoryMappingSetting provides parameters for calculating the memory position of
+//  the relevant fields.
 type MemoryMappingSetting struct {
 	AlignmentBytes int // 1, 2, 4 or 8, 1 for no aligment
 
@@ -185,6 +76,8 @@ type MemoryMappingSetting struct {
 	IDLen    int
 }
 
+// Cache provides an IPC(inter-process communication) bridge with process-based
+// pttbbs process, and shars the cache with board info, user info ...
 type Cache struct {
 	buf []byte
 	*MemoryMappingSetting
@@ -375,10 +268,12 @@ func (c *Cache) caculatePos() {
 	// 	// int     Fbusystate
 }
 
+// NewCache returns Cache (SHM) by connectionString, connectionString indicate the shm location
+// with uri format  eg. shmkey:1228 or file:/tmp/ramdisk/bbs.shm
 func NewCache(connectionString string, settings *MemoryMappingSetting) (*Cache, error) {
-	if strings.HasPrefix(connectionString, "file://") {
+	if strings.HasPrefix(connectionString, "file:") {
 		// mmap
-		filePath := strings.Replace(connectionString, "file://", "", -1)
+		filePath := strings.Replace(connectionString, "file:", "", 1)
 		f, err := os.Open(filePath)
 		// f, err := os.Open("../../../dump.shm")
 		if err != nil {
@@ -413,20 +308,6 @@ func NewCache(connectionString string, settings *MemoryMappingSetting) (*Cache, 
 
 }
 
-// func main() {
-// 	c, err := NewCache("file://../../dump.shm", &MemoryMappingSetting{
-// 		MaxUsers: 50,
-// 		IDLen:    12,
-// 	})
-// 	if err != nil {
-// 		fmt.Println("new cache err:", err)
-// 		return
-// 	}
-// 	fmt.Println("version", c.Version())
-// 	fmt.Println("userid, 0:", c.UserId(0))
-// 	fmt.Println("money, 0:", c.Money(0))
-// }
-
 // Version returns cache (SHM) version of pttbbs, it will be 4842 on pttbbs version
 // 4d56e77 (2009/09 ~ )
 func (c *Cache) Version() uint32 {
@@ -437,14 +318,14 @@ func (c *Cache) Version() uint32 {
 // UserId returns userId string with specific uid, such as "SYSOP",
 // uid means the index in PASSWD file, start with 0.
 func (c *Cache) UserId(uid int) string {
-	// TODO: Check for out of range
+	// TODO: Check if it is out of range
 	s := c.posOfUserId + (c.IDLen+1)*uid
 	return string(bytes.Split(c.buf[s:s+c.IDLen+1], []byte("\x00"))[0])
 }
 
 // Money returns the money user have with specific uid, uid start with 0
 func (c *Cache) Money(uid int) int32 {
-	// TODO: Check for out of range
+	// TODO: Check if it is out of range
 	s := c.posOfMoney + 4*uid
 	return int32(binary.LittleEndian.Uint32(c.buf[s : s+4]))
 }

3fe70a16e14ac0ff6292d6c15ffbea69a0655325
Pichu Chen <pichu@tih.tw>
2021-02-12 18:35:11 +0800

    add docs

---
+++ cmd/bbstool/doc.go
@@ -0,0 +1,18 @@
+/*
+bbstool command provides a command line interface for developer or operators
+control their bbstool database.
+
+**This is an experimental tool**
+
+User can use bbstool from this format:
+
+	bbstool showboardlist
+
+
+The full support list is as following:
+
+    showaccountlist  show full account list [TBD]
+    showboardlist    show full board list [TBD]
+
+*/
+package main

168446042a623782b1c139916b9a9bfcb62085c3
Pichu Chen <pichu@tih.tw>
2021-03-07 16:20:31 +0800

    update systemvshm_linux for lint requriement

--- cache/systemvshm_linux.go
+++ cache/systemvshm_linux.go
@@ -10,17 +10,17 @@ type shmattT uint64
 
 type ShmidDs struct {
 	x struct {
-		_key              keyT
-		uid               uidT
-		gid               gidT
-		cuid              uidT
-		cgid              gidT  // 20
-		mode              int16 // 22
-		__pad1            uint16
-		_seq              int16  // 26
-		__pad2            uint16 // 32
-		__glibc_reserved1 uint64
-		__glibc_reserved2 uint64
+		_key            keyT
+		uid             uidT
+		gid             gidT
+		cuid            uidT
+		cgid            gidT  // 20
+		mode            int16 // 22
+		_Pad1           uint16
+		_seq            int16  // 26
+		_Pad2           uint16 // 32
+		_GlibcReserved1 uint64
+		_GlibcReserved2 uint64
 	} // the length is 48bytes
 	ShmSegsz    sizeT
 	shmAtime    int64

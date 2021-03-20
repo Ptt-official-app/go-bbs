29406079634c163658196f99d42f31a6508066de
Pichu Chen <pichu@tih.tw>
2021-03-20 15:19:27 +0800

    update GetUserArticleRecordFile

--- bbs.go
+++ bbs.go
@@ -157,6 +157,22 @@ type WriteBoardConnector interface {
 	RemoveBoardRecordFileRecord(name string, index uint) error
 }
 
+// UserArticleConnector is a connector for bbs who support cached user article records
+type UserArticleConnector interface {
+
+	// GetUserArticleRecordsPath should return the file path which user article record stores.
+	GetUserArticleRecordsPath(userID string) (string, error)
+
+	// ReadUserArticleRecordFile should return the article record in file.
+	ReadUserArticleRecordFile(name string) ([]ArticleRecord, error)
+
+	// WriteUserArticleRecordFile write user article records into file.
+	WriteUserArticleRecordFile(name string, records []ArticleRecord) error
+
+	// AppendUserArticleRecordFile append user article records into file.
+	AppendUserArticleRecordFile(name string, record ArticleRecord) error
+}
+
 var drivers = make(map[string]Connector)
 
 func Register(drivername string, connector Connector) {
@@ -341,4 +357,51 @@ func (db *DB) ReadBoardRecord(index uint) (*BoardRecord, error) {
 // RemoveBoardRecordFileRecord remove boardRecord brd on index in record file.
 func (db *DB) RemoveBoardRecord(index uint) error {
 	return fmt.Errorf("not implement")
+}
+
+// GetUserArticleRecordFile returns aritcle file which user posted.
+func (db *DB) GetUserArticleRecordFile(userID string) ([]ArticleRecord, error) {
+
+	recs := []ArticleRecord{}
+	uac, ok := db.connector.(UserArticleConnector)
+	if ok {
+
+		path, err := uac.GetUserArticleRecordsPath(userID)
+		if err != nil {
+			log.Println("bbs: open file error:", err)
+			return nil, err
+		}
+		log.Println("path:", path)
+
+		recs, err = uac.ReadUserArticleRecordFile(path)
+		if err != nil {
+			log.Println("bbs: ReadUserArticleRecordFile error:", err)
+			return nil, err
+		}
+		if len(recs) != 0 {
+			return recs, nil
+		}
+
+	}
+
+	boardRecords, err := db.ReadBoardRecords()
+	if err != nil {
+		log.Println("bbs: ReadBoardRecords error:", err)
+		return nil, err
+	}
+
+	for _, r := range boardRecords {
+		ars, err := db.ReadBoardArticleRecordsFile(r.BoardID())
+		if err != nil {
+			log.Println("bbs: ReadBoardArticleRecordsFile error:", err)
+			return nil, err
+		}
+		for _, ar := range ars {
+			if ar.Owner() == userID {
+				recs = append(recs, ar)
+			}
+		}
+	}
+
+	return recs, nil
 }

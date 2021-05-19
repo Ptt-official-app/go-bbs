88f5e0fe87cb2a7d730b87fc103f103ca79c5abd
Lex Liu <lexamo.liu@gmail.com>
2021-05-19 13:45:50 +0800

    Remove unuse ANSI bytes filter function
    The function is replaced by FilterStringANSI function

--- ansi_test.go
+++ ansi_test.go
@@ -1,22 +1,9 @@
 package bbs
 
 import (
-	"bytes"
 	"strings"
 	"testing"
 )
-
-func TestFilterANSI(t *testing.T) {
-	src := []byte("[1;31m→ [33mpichu2[m[33m:推")
-	dst := make([]byte, len(src))
-	expected := []byte("→ pichu2:推")
-
-	dst = FilterANSI(dst, src)
-
-	if bytes.Compare(expected, dst) != 0 {
-		t.Errorf("FilterANSI doesn't filter the ANSI code, \nexpected: \n%s, \ngot: \n%s", expected, dst)
-	}
-}
 
 func TestFilterStringANSI(t *testing.T) {
 	src := "[1;31m→ [33mpichu2[m[33m:推"
@@ -29,13 +16,12 @@ func TestFilterStringANSI(t *testing.T) {
 	}
 }
 
-func BenchmarkReflect(b *testing.B) {
-	src := []byte("[1;31m→ [33mpichu2[m[33m:推")
-	dst := make([]byte, len(src))
+func BenchmarkStringANSI(b *testing.B) {
+	src := "[1;31m→ [33mpichu2[m[33m:推"
 
 	b.ResetTimer()
 
 	for i := 0; i < b.N; i++ {
-		FilterANSI(dst, src)
+		_ = FilterStringANSI(src)
 	}
 }
